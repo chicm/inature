674208417195ba1a3fc0f5b4b7444b126ff10450
chicm <idv2005@gmail.com>
2019-04-17 11:37:47 +0000

    updates

---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+DATA_DIR = '/mnt/data/inature'
+TRAIN_IMG_DIR = os.path.join(DATA_DIR, 'train_val_224')
+TEST_IMG_DIR = os.path.join(DATA_DIR, 'test_224')
+MODEL_DIR = os.path.join(DATA_DIR, 'models')
